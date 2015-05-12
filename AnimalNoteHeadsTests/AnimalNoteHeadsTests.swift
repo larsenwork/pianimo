5c19ead53f0db5915fd32447885dab0804e097a2
Andreas Larsen <andreaslarsen86@gmail.com>
2015-05-12 18:28:09 +0200

    Initial Commit

---
+++ AnimalNoteHeadsTests/AnimalNoteHeadsTests.swift
@@ -0,0 +1,36 @@
+//
+//  AnimalNoteHeadsTests.swift
+//  AnimalNoteHeadsTests
+//
+//  Created by Andreas Larsen on 12/05/15.
+//  Copyright (c) 2015 AnimalNoteHeads. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class AnimalNoteHeadsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
